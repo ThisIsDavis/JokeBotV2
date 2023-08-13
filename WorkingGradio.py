f1ee804ab9dcafb5f00ab965b8d32c01f04c9f9e
Wongjunkit <bryan.wongjunkit@gmail.com>
2023-08-13 22:31:26 +0800

    BETTER VOICE RECOGNITION DONE

--- WorkingGradio.py
+++ WorkingGradio.py
@@ -1,5 +1,6 @@
 import gradio as gr
 import os
+import speech_recognition as sr
 from llama_index import SimpleDirectoryReader
 # from llama_index.readers.file.docs_parser import PDFParser
 from llama_index.readers.file.docs_reader import PDFReader
@@ -7,8 +8,7 @@ from llama_index.readers.schema.base import Document
 from llama_index import GPTVectorStoreIndex, LLMPredictor, PromptHelper, ServiceContext
 from langchain import ConversationChain, OpenAI
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from transformers import pipeline
-from textblob import TextBlob
+
 
 class GPTProcessing(object):
     
@@ -24,8 +24,6 @@ class GPTProcessing(object):
         self.index_status = "Error: Index is not selected"
         self.index_setup_result = ".... no action..."
         self.OPENAI_API_KEY = "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"
-        # Pre-trained voice recognition model.
-        self.voice_recognition_model = pipeline("automatic-speech-recognition")
 
     def create_ui(self):
         with self.ui_obj:
@@ -41,8 +39,8 @@ class GPTProcessing(object):
                 message.submit(self.chat, [chatbot, message], chatbot)
                 # Voice Recognition
                 with gr.Row():
-                    # Record audio
-                    voice_recog = gr.Audio(source = "microphone", type="filepath")
+                    # Record audio and output the audio filepath.
+                    voice_recog = gr.Audio(source = "microphone", type = "filepath")
                     # Button to start voice recognition
                     voice_recog_action = gr.Button("Keyword Voice Recognition")
             with gr.TabItem("User Joke Preference Learning"):
@@ -52,7 +50,7 @@ class GPTProcessing(object):
                     gr.Textbox(label="Enter your joke preferences here:")
                     gr.Button("Submit")
 
-    # Button to start recording voice and outputting it to the text box.
+    # Button to start recording voice and outputting it to the message text box.
             voice_recog_action.click(
                 self.transcribe_audio,
                 [
@@ -96,21 +94,33 @@ class GPTProcessing(object):
             yield chat_history + [(user_input, response)]
 
     # Voice recognition.
-    def transcribe_audio(self, audio):
-        # Recognise audio input.
-        text = self.voice_recognition_model(audio)["text"]
+    def transcribe_audio(self, audio_path: str) -> str:
+        """
+        Takes in the filepath where the audio is stored and use an Automatic Speech Recognition Model to transcribe the audio into text.
+        Will take the first word of the whole transcribed sentence.
+        :Input:
+            audio_path: The filepath where the audio is stored.
+        :Output:
+            text: The transcribed text based on the input audio.
+        """
+        # Initalise a Recognizer instance.
+        recogniser = sr.Recognizer()
         
-        # If no audio received, give error.
-        if len(text) == 0 or text.isspace():
-            result = "Error: Unable to recognise audio"
-        # Else split the text and returned only the first word.
-        else:
-            text = str(text.split()[0])
-            # Autocorrect any spelling mistakes.
-            result = TextBlob(text)
-            result = result.correct()
+        # Open the audio file based on inputted filepath an initalise it to source variable.
+        with sr.AudioFile(audio_path) as source:
+            # Extract audio data from the source file.
+            audio = recogniser.record(source)
             
-        return result
+            # If there's audio, recognise the speech using Google Speech Recognition and return the first keyword transcribed.
+            try:
+                text = recogniser.recognize_google(audio)   # Transcribe the audio into text.
+                text = str(text.split()[0]).lower()         # Extract the first word of the text and then turn it to lowercase.
+                return text
+            
+            # Speech is unintelligible or other errors, return error text.
+            except:
+                text = "Error: Unable to recognise audio"
+                return text
     
 
 if __name__ == '__main__':
