e3113609bb8eb489d3b08550dd3f11b884142fca
ptay00101 <ptay0010@student.monash.edu>
2023-09-15 17:30:46 +0800

    key removed

--- WorkingGradio.py
+++ WorkingGradio.py
@@ -26,7 +26,7 @@ class GPTProcessing(object):
         self.input_my = None
         self.output_my = None
 
-        self.OPENAI_API_KEY = "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"
+        self.OPENAI_API_KEY = ""
         os.environ["OPENAI_API_KEY"] = self.OPENAI_API_KEY
         openai.api_key = self.OPENAI_API_KEY
 
